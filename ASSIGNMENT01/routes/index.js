8e565df09d07a68f6ddb7ddc4a6d422058ed487a
Vrajkumar Bagadiya <96316547+github18122021@users.noreply.github.com>
2024-06-10 23:15:57 -0400

    Created layout ui
    created shared header and footer ui, added animatated logo.

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -3,22 +3,22 @@ var router = express.Router();
 
 /* GET home page  */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Home' });
+  res.render('home', { title: 'Home', activePage: 'home' });
 });
 
 // GET /about
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Me' });
+  res.render('about', { title: 'About Me', activePage: 'about'});
 });
 
 // GET /projects
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+  res.render('projects', { title: 'Projects', activePage: 'projects'});
 });
 
 // GET /contact
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Me' });
+  res.render('contact', { title: 'Contact Me', activePage: 'contact'});
 });
 
 
