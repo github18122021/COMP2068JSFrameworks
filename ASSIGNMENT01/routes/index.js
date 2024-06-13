1e1d2ca26b16ba75ad888ea89e6906cae8015dbf
Vrajkumar Bagadiya <96316547+github18122021@users.noreply.github.com>
2024-06-13 01:03:11 -0400

    Added projects in project section
    Changed font to give a slightly comic look,
    
    listed projects in project section, created their separated ui for in depth demonstration.
    
    added functionality to check live demo of a project, and source code.

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -21,6 +21,46 @@ router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact Me', activePage: 'contact'});
 });
 
+// /projects/[projectName]
+
+router.get('/projects/bookstore', function(req, res, next) {
+  res.render('projects/bookstore', { title: 'Book Store'});
+});
+
+router.get('/projects/guessGame', function(req, res, next) {
+  res.render('projects/guessGame', { title: 'Guess Game'});
+});
+
+router.get('/projects/currencyCalculator', function(req, res, next) {
+  res.render('projects/currencyCalculator', { title: 'Currency Calculator'});
+});
+
+router.get('/projects/ageCalculator', function(req, res, next) {
+  res.render('projects/ageCalculator', { title: 'Age Calculator'});
+});
+
+router.get('/projects/weather', function(req, res, next) {
+  res.render('projects/weather', { title: 'Weather'});
+});
+
+router.get('/projects/textReader', function(req, res, next) {
+  res.render('projects/textReader', { title: 'Text Reader'});
+});
+
+router.get('/projects/choicePicker', function(req, res, next) {
+  res.render('projects/choicePicker', { title: 'Choice Picker'});
+});
+
+router.get('/projects/passwordGenerator', function(req, res, next) {
+  res.render('projects/passwordGenerator', { title: 'Password Generator'});
+});
+
+router.get('/projects/calculator', function(req, res, next) {
+  res.render('projects/calculator', { title: 'Calculator'});
+});
+
+
+
 
 
 module.exports = router;
